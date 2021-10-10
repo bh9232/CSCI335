841e7d225582cdee7825e026dc1577e3e1c2b624
bh9232 <bhong9232@gmail.com>
2021-10-09 21:04:25 -0400

    modified

--- sequence_map.cc
+++ sequence_map.cc
@@ -29,7 +29,7 @@ friend ostream& operator<<(ostream &out, const SequenceMap &another_map){
 
 void SequenceMap::Merge(const SequenceMap &other_sequence){
     if(recognition_sequence_ == other_sequence.recognition_sequence_){
-        for(size_t i = 0, i < other_sequence.enzyme_acronyms_.size(); i++){
+        for(size_t i = 0, i < other_sequence.enzyme_acronyms_.size(); ++i){
             enzyme_acronyms_.push_back(other_sequence.enzyme_acronyms_[i]);
         }
     }
